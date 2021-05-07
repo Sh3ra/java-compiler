c113428bcc80d4f7190943e44e00f17860191775
QuartzKrystal <maroashraf428@gmail.com>
2021-05-07 14:42:58 +0200

    Adding Regex Generation Strategy Classes

---
+++ RegexGenerationStrategy.h
@@ -0,0 +1,17 @@
+//
+// Created by marwan_ashraf on 06-May-21.
+//
+
+#ifndef JAVA_COMPILER_REGEXGENERATIONSTRATEGY_H
+#define JAVA_COMPILER_REGEXGENERATIONSTRATEGY_H
+
+
+#include <vector>
+
+class RegexGenerationStrategy {
+public:
+    virtual RegularExpression generateRegex(RegularExpression &regex, vector <RegularDefinition> regDefList) = 0;
+};
+
+
+#endif //JAVA_COMPILER_REGEXGENERATIONSTRATEGY_H
